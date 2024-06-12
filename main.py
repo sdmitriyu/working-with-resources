f241f273ac43621719966825030fba4cdf8a5072
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-06-12 19:23:02 +0300

    База данных

--- main.py
+++ main.py
@@ -12,18 +12,27 @@ rows = table.find_all('tr')
 with open('crypto_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile)
 
-    # Запишем заголовки столбцов в CSV файл
+    # Write column headers to CSV file
     csvwriter.writerow(['Rank', 'Name', 'Price', 'Market Cap', 'Volume', 'Change'])
 
-    for row in rows[1:101]:  # пропустим заголовок
+    for row in rows[1:101]:  # skipping the header
         cells = row.find_all('td')
-        rank = cells[0].text.strip()
-        name = cells[1].text.strip()
-        price = cells[3].text.strip()
-        market_cap = cells[6].text.strip()
-        volume = cells[7].text.strip()
-        change = cells[8].text.strip()
 
-        csvwriter.writerow([rank, name, price, market_cap, volume, change])
+        # Check if there are enough cells to extract data from
+        if len(cells) >= 7:
+            rank = cells[0].text.strip()
 
-print('Данные успешно записаны в crypto_data.csv')
+            # Check if '<p>' tag exists before extracting text
+            if cells[2].find('p'):
+                name = cells[2].find('p').text.strip()
+            else:
+                name = cells[2].text.strip()
+
+            price = cells[3].text.strip()
+            market_cap = cells[4].text.strip()
+            volume = cells[5].text.strip()
+            change = cells[6].text.strip()
+
+            csvwriter.writerow([rank, name, price, market_cap, volume, change])
+
+print('Data has been successfully written to crypto_data.csv')
